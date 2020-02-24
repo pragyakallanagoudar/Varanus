26779059400764ec5e3e7351d24b53d24a6abee5
pragyakallanagoudar <JustinSeagull123>
2020-02-24 13:33:29 -0800

    Introduced AnimalsPage recycler view.

--- app/src/main/java/com/pragyakallanagoudar/varanus/AnimalsPage.java
+++ app/src/main/java/com/pragyakallanagoudar/varanus/AnimalsPage.java
@@ -1,19 +1,96 @@
 package com.pragyakallanagoudar.varanus;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.pragyakallanagoudar.varanus.adapter.AnimalsAdapter;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class AnimalsPage extends AppCompatActivity {
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
-    public Button[] species_buttons; // a button for every single species of animal
+import android.util.Log;
+import android.view.View;
+
+
+
+public class AnimalsPage extends AppCompatActivity implements
+        View.OnClickListener,
+        AnimalsAdapter.OnCheckBoxSelectedListener {
+
+    private static final String TAG = "MainActivity";
+    private FirebaseFirestore mFirestore;
+    private Query mQueryTasks, mQueryEnclosureLog;
+    private RecyclerView mAnimalsRecycler;
+    private AnimalsAdapter mAdapter;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animals_page);
+        mAnimalsRecycler = findViewById(R.id.animal_recyclerview);
+        initFirestore();
+        initRecyclerView();
+
+    }
+    private void initFirestore() {
+        mFirestore = FirebaseFirestore.getInstance();
+        mQueryTasks = mFirestore.collection("Tasks")
+                .limit(50)
+                .orderBy("species");
     }
 
-    //when you click on one of the buttons send in the identifying value with the intent
+    private void initRecyclerView() {
+
+        mAdapter = new AnimalsAdapter(mQueryTasks, this) {
+
+            @Override
+            protected void onDataChanged() {
+                // Show/hide content if the query returns empty.
+
+                mAnimalsRecycler.setVisibility(View.VISIBLE);
+
+            }
+
+            //@Override
+            protected void onError(FirebaseFirestoreException e) {
+                // Show a snackbar on errors
+                Snackbar.make(findViewById(android.R.id.content),
+                        "Error: check logs for info.", Snackbar.LENGTH_LONG).show();
+            }
+        };
+
+        mAnimalsRecycler.setLayoutManager(new LinearLayoutManager(this));
+        mAnimalsRecycler.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (mAdapter != null) {
+            mAdapter.startListening();
+        }
+    }
+
+    public void onClick (View view)
+    {
+
+    }
+
+    @Override
+    public void onCheckBoxSelected(DocumentSnapshot tasks)
+    {
+        Intent intent = new Intent(this, TaskDetailActivity.class);
+        intent.putExtra(TaskDetailActivity.KEY_TASK_ID, tasks.getId());
+        startActivity(intent);
+
+    }
 
 }
---
+++ app/src/main/java/com/pragyakallanagoudar/varanus/adapter/AnimalsAdapter.java
@@ -0,0 +1,89 @@
+package com.pragyakallanagoudar.varanus.adapter;
+
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.Query;
+import com.pragyakallanagoudar.varanus.R;
+import com.pragyakallanagoudar.varanus.model.Task;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+/* RecyclerView adapter for a list of Tasks.
+ */
+
+public class AnimalsAdapter extends FirestoreAdapter<AnimalsAdapter.ViewHolder> {
+
+    public interface OnCheckBoxSelectedListener
+    {
+        void onCheckBoxSelected(DocumentSnapshot tasks); // implement in subclasses
+    }
+
+    private OnCheckBoxSelectedListener mListener;  // instance of above interface
+
+    // constructor
+    public AnimalsAdapter(Query query, OnCheckBoxSelectedListener listener)
+    {
+        super(query);
+        mListener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        return new ViewHolder(inflater.inflate(R.layout.item_animal, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(getSnapshot(position), mListener);
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView enclosureView;
+        TextView animalView;
+        ImageView animalImageView;
+        ImageView animalStatusView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            enclosureView = itemView.findViewById(R.id.enclosure_name);
+            animalView = itemView.findViewById(R.id.animal_name);
+            animalImageView = itemView.findViewById(R.id.animal_image);
+            animalStatusView = itemView.findViewById(R.id.animal_status);
+        }
+
+        public void bind(final DocumentSnapshot snapshot,
+                         final OnCheckBoxSelectedListener listener) {
+
+            Task Tasks = snapshot.toObject(Task.class);
+            Glide.with(animalImageView.getContext())
+                    .load(Tasks.getPhoto())
+                    .into(animalImageView);
+            enclosureView.setText(Tasks.getEnclosure());
+            animalView.setText(Tasks.getSpecies());
+            animalStatusView.setBackgroundResource(R.drawable.ic_check);
+
+            // Click listener
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener != null) {
+                        listener.onCheckBoxSelected(snapshot);
+                    }
+                }
+            });
+        }
+
+    }
+}
+
